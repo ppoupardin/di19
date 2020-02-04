77d1f0f7f8208902d78ed49679298c38af4e057e
Poupardin Pierre-Emmanuel <pierreemmanuel.poupardin@viacesi.fr>
2020-02-04 16:09:23 +0100

    Contenu master.css dans textearea page admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,7 +3,15 @@ namespace src\Controller;
 
 class AdminController extends AbstractController {
     public function index(){
-        //Lancer la vue TWIG
-        return $this->twig->render('Admin/index_admin.html.twig');
+        $file='master.css';
+        $dataCss = file_get_contents('./asset/css/'.$file);
+        //Lancer la vue TWIG page Admin
+        return $this->twig->render('Admin/index_admin.html.twig', [
+            'cssFileData' => $dataCss
+        ]);
+    }
+
+    public function sendCss(){
+        // fonction enregistrement fichier CSS
     }
 }
