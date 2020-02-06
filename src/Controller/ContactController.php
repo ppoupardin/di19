77b93c4f6d687e274465f2bc66f5e351ad743fba
Langlois William <Langlois.William.pro@gmail.com>
2020-02-06 12:45:48 +0100

    Faille XSS : AdminController,ContactController + refonte formulaire de contact + page de confirmation de l'envoi d'email + bouton nous contacter dans l'index + catch exception dans l'envoi d'email + changement port de l'email vers 2525

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -8,32 +8,68 @@ class ContactController extends AbstractController{
     public function __construct()
     {
         parent::__construct();
-        $this->transport = (new \Swift_SmtpTransport('smtp.mailtrap.io', 25))
-            ->setUsername('3dd84281bc8679')
-            ->setPassword('8a9180301c670a');
+        $this->transport = (new \Swift_SmtpTransport('smtp.mailtrap.io', 2525))
+            //mon mailtrap encoyez pas tous des mails là
+            ->setUsername('ed56f41e29d67f')
+            ->setPassword('6d79a526116781');
         $this->mailer = new \Swift_Mailer($this->transport);
 
     }
 
     public function showForm(){
+        unset($_SESSION['errorcontactform']);
         return $this->twig->render('Contact/form.html.twig');
     }
 
     public function sendMail(){
-        $mail = (new \Swift_Message('Contact depuis le formulaire'))
+        if(($_POST['email'])!=(trim($_POST['email']))){
+            $_SESSION['errorcontactform'] = "L'Email ne peut commencer ou finir par un espace";
+            header('Location:/Contact');
+            return;
+        }
+        if(trim($_POST['email']) != strip_tags(trim($_POST['email']))) {
+            $_SESSION['errorcontactform'] = "L'Email n'a pas une structure valide";
+            header('Location:/Contact');
+            return;
+        }
+
+        if(($_POST['nom'])!=(trim($_POST['nom']))){
+            $_SESSION['errorcontactform'] = "Votre nom ne peut commencer ou finir par un espace";
+            header('Location:/Contact');
+            return;
+        }
+        if(trim($_POST['nom']) != strip_tags(trim($_POST['nom']))) {
+            $_SESSION['errorcontactform'] = "Votre nom n'a pas une structure valide";
+            header('Location:/Contact');
+            return;
+        }
+
+        if(trim($_POST['content']) != strip_tags(trim($_POST['content']))) {
+            $_SESSION['errorcontactform'] = "Le contenu de votre message n'a pas une structure valide ( balises :'<' ,'>' interdites)";
+            header('Location:/Contact');
+            return;
+        }
+    try {
+        $mail = (new \Swift_Message('Contact à partir du formulaire'))
             ->setFrom([$_POST["email"] => $_POST["nom"]])
-            ->setTo('contact@monsite.fr')
+            ->setTo('contact@PWB_Domo.fr')
             ->setBody(
                 $this->twig->render('Contact/mail.html.twig',
                     [
                         'message' => $_POST["content"]
                     ])
-                ,'text/html'
+                , 'text/html'
             );
-
         $result = $this->mailer->send($mail);
+    }catch (\Exception $err1) {
+        return $this->twig->render('Contact/confirmation.html.twig',[
+            'result'=>0
+        ]);
+    }
 
-        return $result;
+        return $this->twig->render('Contact/confirmation.html.twig',[
+            'result'=>$result
+        ]);
     }
 
 }
