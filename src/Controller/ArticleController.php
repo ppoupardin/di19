e42bff3ee024585f1002f0d3698954ea8112bc33
Poupardin Pierre-Emmanuel <pierreemmanuel.poupardin@viacesi.fr>
2020-02-06 22:49:19 +0100

    corection

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -216,26 +216,31 @@ class ArticleController extends AbstractController {
     }
 
     public function fixtures(){
+        ArticleController::roleNeed('administrateur');
         $arrayAuteur = array('Fabien', 'Brice', 'Bruno', 'Jean-Pierre', 'Benoit', 'Emmanuel', 'Sylvie', 'Marion');
         $arrayTitre = array('PHP en force', 'React JS une valeur montante', 'C# toujours au top', 'Java en légère baisse'
         , 'Les entreprises qui recrutent', 'Les formations à ne pas rater', 'Les langages populaires en 2020', 'L\'année du Javascript');
         $arrayStatus = array('0',"1","2");
+        $arrayCategorie = array('0',"1","2","3","4","5","6",11);
         $dateajout = new DateTime();
         $article = new Article();
-        $article->SqlTruncate(BDD::getInstance());
+        $categorie = new Categorie();
+        $article->SqlTruncate(Bdd::getInstance());
+
         for($i = 1;$i <=35; $i++){
             shuffle($arrayAuteur);
             shuffle($arrayTitre);
             shuffle($arrayStatus);
+            shuffle($arrayCategorie);
 
             $dateajout->modify('+'.$i.' day');
 
-            $article->setTitre($arrayTitre[0])
-                ->setDescription('On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L\'avantage du Lorem Ipsum sur un texte générique comme \'Du texte. Du texte. Du texte.\' est qu\'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour \'Lorem Ipsum\' vous conduira vers de nombreux sites qui n\'en sont encore qu\'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d\'y rajouter de petits clins d\'oeil, voire des phrases embarassantes).')
-                ->setDateAjout($dateajout->format('Y-m-d'))
-                ->setAuteur($arrayAuteur[0])
-                ->setStatus($arrayStatus[0])
-            ;
+            $article->setTitre($arrayTitre[0]);
+            $article->setDescription('On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L\'avantage du Lorem Ipsum sur un texte générique comme \'Du texte. Du texte. Du texte.\' est qu\'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour \'Lorem Ipsum\' vous conduira vers de nombreux sites qui n\'en sont encore qu\'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d\'y rajouter de petits clins d\'oeil, voire des phrases embarassantes).');
+            $article->setDateAjout($dateajout->format('Y-m-d'));
+            $article->setAuteur($arrayAuteur[0]);
+            $article->setStatus($arrayStatus[0]);
+            $article->setIdcat($arrayCategorie[0]);
 
             $article->SqlAdd(BDD::getInstance());
         }
